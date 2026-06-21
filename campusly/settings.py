acc14cdfd1d85e6c7635c90b4f3862214bb64ad0
Zeenat Sayyed <zeenatsayyed692@gmail.com>
2026-06-21 12:41:47 +0530

    Add Vercel URL to CORS

--- campusly/settings.py
+++ campusly/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "https://campusly-frontend-six.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
